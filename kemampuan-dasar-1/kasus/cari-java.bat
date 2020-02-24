8d7a764d160eb1eed0a0d974e997de090bd63ed1
Andris Iswahyudi <andrisiswahyudi185@gmail.com>
2020-02-24 23:25:43 +0700

    menambahkan file baru

---
+++ kemampuan-dasar-1/kasus/cari-java.bat
@@ -0,0 +1,10 @@
+PS C:\> Get-ChildItem 'C:\Program Files (x86)\Java'
+
+
+    Directory: C:\Program Files (x86)\Java
+
+
+Mode                LastWriteTime         Length Name                                              
+----                -------------         ------ ----                                              
+da----       01/03/2019      5:12                jdk1.8.0_201                                      
+d-----       15/01/2020     11:00                jre1.8.0_241 
